d9c1b2a0e6360dfcfcb2768bf5071c55fe15680a
Jesper Smith <jsmith@ihmc.us>
2017-02-15 12:44:56 -0600

    Added pub/sub types for serialization

---
+++ generated/us/ihmc/idl/sensor_msgs/HeaderPubSubType.java
@@ -0,0 +1,53 @@
+package us.ihmc.idl.sensor_msgs;
+
+import java.io.IOException;
+
+import us.ihmc.rtps.common.SerializedPayload;
+import us.ihmc.rtps.TopicDataType;
+import us.ihmc.idl.CDR;
+
+public class HeaderPubSubType implements TopicDataType<Header>
+{
+	public static final String name = "sensor_msgs::Header";
+	private final CDR serializeCDR = new CDR();
+	private final CDR deserializeCDR = new CDR();
+	
+	
+	
+    public HeaderPubSubType()
+    {
+        
+    }
+    
+       @Override
+   public void serialize(Header data, SerializedPayload serializedPayload) throws IOException
+   {
+      serializeCDR.init(serializedPayload);
+      data.serialize(serializeCDR);
+   }
+
+   @Override
+   public void deserialize(SerializedPayload serializedPayload, Header data) throws IOException
+   {
+      deserializeCDR.init(serializedPayload);
+      data.deserialize(deserializeCDR);
+   }
+
+   @Override
+   public int getTypeSize()
+   {
+      return Header.getMaxCdrSerializedSize();
+   }
+
+   @Override
+   public String getName()
+   {
+      return name;
+   }
+
+   @Override
+   public Header createData()
+   {
+      return new Header();
+   }
+}
---
+++ generated/us/ihmc/idl/sensor_msgs/TimePubSubType.java
@@ -0,0 +1,53 @@
+package us.ihmc.idl.sensor_msgs;
+
+import java.io.IOException;
+
+import us.ihmc.rtps.common.SerializedPayload;
+import us.ihmc.rtps.TopicDataType;
+import us.ihmc.idl.CDR;
+
+public class TimePubSubType implements TopicDataType<Time>
+{
+	public static final String name = "sensor_msgs::Time";
+	private final CDR serializeCDR = new CDR();
+	private final CDR deserializeCDR = new CDR();
+	
+	
+	
+    public TimePubSubType()
+    {
+        
+    }
+    
+       @Override
+   public void serialize(Time data, SerializedPayload serializedPayload) throws IOException
+   {
+      serializeCDR.init(serializedPayload);
+      data.serialize(serializeCDR);
+   }
+
+   @Override
+   public void deserialize(SerializedPayload serializedPayload, Time data) throws IOException
+   {
+      deserializeCDR.init(serializedPayload);
+      data.deserialize(deserializeCDR);
+   }
+
+   @Override
+   public int getTypeSize()
+   {
+      return Time.getMaxCdrSerializedSize();
+   }
+
+   @Override
+   public String getName()
+   {
+      return name;
+   }
+
+   @Override
+   public Time createData()
+   {
+      return new Time();
+   }
+}
