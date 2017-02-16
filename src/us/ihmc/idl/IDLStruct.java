5eecd76ec121de66b615e9b987cdd466eaa33c10
Jesper Smith <jsmith@ihmc.us>
2017-02-16 10:52:06 -0600

    Cleaned up formatting of generated class. Made all elements pre-allocated

--- src/us/ihmc/idl/IDLStruct.java
+++ src/us/ihmc/idl/IDLStruct.java
@@ -1,5 +1,7 @@
 package us.ihmc.idl;
 
+import org.apache.commons.lang3.NotImplementedException;
+
 /**
  * Represents and IDL struct.  
  * 
@@ -7,19 +9,27 @@ package us.ihmc.idl;
  * @author Jesper Smith
  *
  */
-public interface IDLStruct
+public interface IDLStruct <T extends IDLStruct<?>>
 {
    /**
     * Serialization method used internally to serialize nested structs
     * 
     * @param cdr
     */
-   void deserialize(CDR cdr);
+   default void deserialize(CDR cdr) { throw new NotImplementedException("Not implemented"); };
    
    /**
     * Deserialization method used internally to deserialize nested structs
     * 
     * @param cdr
     */
-   void serialize(CDR cdr);
+   default void serialize(CDR cdr) { throw new NotImplementedException("Not implemented"); };
+   
+   
+   /**
+    * Setter method used internally to set nested struct sequences.
+    * 
+    * @param other
+    */
+   default void set(T other) { throw new NotImplementedException("Not implemented"); };
 }
--- test/us/ihmc/idl/IDLElement/IDLElementTestTest.java
+++ test/us/ihmc/idl/IDLElement/IDLElementTestTest.java
@@ -7,7 +7,6 @@ import org.junit.Test;
 import us.ihmc.idl.generated.IDLElement.Color;
 import us.ihmc.idl.generated.IDLElement.IDLElementTest;
 import us.ihmc.idl.generated.IDLElement.IDLElementTestPubSubType;
-import us.ihmc.idl.generated.IDLElement.NestedElement;
 import us.ihmc.rtps.common.SerializedPayload;
 
 public class IDLElementTestTest
@@ -17,21 +16,7 @@ public class IDLElementTestTest
    {
       IDLElementTest test = new IDLElementTest();
       test.setColorTest(Color.blue);
-      for(int i = 0; i < test.getStringArray().length; i++)
-      {
-         test.getStringArray()[i] = new StringBuilder("Bla");
-      }
       
-      
-      
-      for(int a = 0; a < test.getNestedArray().length; ++a)
-      {
-          for(int b = 0; b < test.getNestedArray()[a].length; ++b)
-          {
-             test.getNestedArray()[a][b] = new NestedElement();
-          }
-      }
-
       
       IDLElementTestPubSubType type = new IDLElementTestPubSubType();
       SerializedPayload payload = new SerializedPayload(type.getTypeSize());
