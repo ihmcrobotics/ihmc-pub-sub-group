58f6c6dae265407c1f7ca15fcb309c0c4a591f32
Jesper Smith <jsmith@ihmc.us>
2017-02-16 14:24:43 -0600

    Created passing comparison test for IDLElementTest using c++ generated code and java code.

---
+++ cppsrc/test/idlelementserializetest.cpp
@@ -0,0 +1,93 @@
+#include "IDLElementTest.h"
+#include "IDLElementTestPubSubTypes.h"
+#include <iostream>
+#include <iomanip>
+#include <string>
+int main()
+{
+
+    IDLElement::IDLElementTest msg;
+    msg.charTest('3');
+    msg.wcharTest(L'Ω');
+    msg.octetTest(0xa);
+    msg.shortTest(-16);
+    msg.ushortTest(15);
+    msg.longTest(-58102);
+    msg.ulongTest(914);
+    msg.longlongTest(-90224141);
+    msg.ulonglongTest(582142);
+    msg.floatTest(258145.2143f);
+    msg.doubleTest(9184289051.1241);
+    msg.booleanTest(true);
+    msg.colorTest(IDLElement::Color::blue);
+    msg.nestedElementTest().longTest(518);
+    msg.nestedElementTest().stringTest("Nested");
+    msg.stringTest("Wolololo");
+
+    for(int i = 0; i < 10; i++)
+      {
+         msg.longArray()[i] = i * 124;
+      }
+
+    for(int a = 0; a < 5; a++)
+      {
+         for(int b = 0; b < 3; b++)
+         {
+            msg.nestedArray()[a][b].longTest(a + a * b * 3 + b + 24);
+            msg.nestedArray()[a][b].stringTest("arrayDim:" + std::to_string(a) + std::to_string(b));
+         }
+      }
+
+    for (int i = 0; i < 4; i++)
+      {
+         msg.stringArray()[i].append("arrayDim:" + std::to_string(i));
+      }
+
+    int c = 0;
+    for (int s = 1; s < 4; s++)
+      {
+         for (int w = 0; w < 5; w++)
+         {
+            int i = s + s * w;
+            switch (s)
+            {
+            case 1:
+               msg.charSeqTest().push_back(std::to_string(i).at(0));
+               msg.wcharSeqTest().push_back(L'Ω');
+               msg.octetSeqTest().push_back(i * 2);
+               msg.shortSeqTest().push_back(-2 * i + 1);
+               msg.booleanSeqTest().push_back(i % 2 == 0);
+            case 2:
+               msg.ushortSeqTest().push_back(i + 4);
+               msg.longSeqTest().push_back(i * 124 - 98);
+               msg.ulongSeqTest().push_back(i * 11561);
+               msg.longlongSeqtest().push_back(-2143125l + i * 1251);
+            case 3:
+               msg.ulonglongSeqTest().push_back(241l + i * 100421410l);
+               msg.floatSeqTest().push_back(325.25f * i);
+               msg.doubleSeqTest().push_back(15095.921 * i);
+               IDLElement::NestedElement nested;
+               nested.longTest(i * 541);
+               nested.stringTest("dim:" + std::to_string(s) + std::to_string(w));
+               msg.nestedSeqTest().push_back(nested);
+               msg.stringSeqTest().push_back("sdim:" + std::to_string(s) + std::to_string(w));
+
+            }
+         }
+      }
+
+    IDLElement::IDLElementTestPubSubType type;
+    SerializedPayload_t payload(IDLElement::IDLElementTest::getMaxCdrSerializedSize());
+    type.serialize(&msg,&payload);
+
+    std::cout << "static byte[] cppData = {" << std::setfill('0');
+    for(int i = 0; i < payload.length; i++)
+    {
+        if(i > 0)
+        {
+            std::cout << ", ";
+        }
+        std::cout << "(byte)0x" << std::hex  << std::setw(2) << ((int)payload.data[i]);
+    }
+    std::cout << "};" << std::endl;
+}
--- test/us/ihmc/idl/IDLElement/IDLElementTestTest.java
+++ test/us/ihmc/idl/IDLElement/IDLElementTestTest.java
@@ -9,42 +9,122 @@ import org.junit.Test;
 import us.ihmc.idl.generated.IDLElement.Color;
 import us.ihmc.idl.generated.IDLElement.IDLElementTest;
 import us.ihmc.idl.generated.IDLElement.IDLElementTestPubSubType;
+import us.ihmc.idl.generated.IDLElement.NestedElement;
 import us.ihmc.rtps.common.SerializedPayload;
 
 public class IDLElementTestTest
 {
-   @Test
-   public void testIDLElementTest() throws IOException
+   //Output of the C++ program SerializationTest, found in cppsrc/test
+
+   private void populateIDLElementTest(IDLElementTest test)
    {
-      IDLElementTest test = new IDLElementTest();
-      
+
       test.setCharTest('3');
       test.setWcharTest('Ω');
       test.setOctetTest((byte) 0xa);
-      test.setShortTest((short)-16);
+      test.setShortTest((short) -16);
       test.setUshortTest(15);
       test.setLongTest(-58102);
       test.setUlongTest(914);
       test.setLonglongTest(-90224141);
       test.setUlonglongTest(582142);
-      test.setFloatTest(0258145.2143f);
+      test.setFloatTest(258145.2143f);
       test.setDoubleTest(9184289051.1241);
       test.setBooleanTest(true);
-      test.setColorTest(Color.green);
+      test.setColorTest(Color.blue);
+      test.getNestedElementTest().setLongTest(518);
+      test.getNestedElementTest().getStringTest().append("Nested");
       test.getStringTest().append("Wolololo");
-      test.getWstringTest().append("ΑΩ");
-      
-      
-      
+
+      for (int i = 0; i < 10; i++)
+      {
+         test.getLongArray()[i] = i * 124;
+      }
+
+      for (int a = 0; a < 5; a++)
+      {
+         for (int b = 0; b < 3; b++)
+         {
+            test.getNestedArray()[a][b].setLongTest(a + a * b * 3 + b + 24);
+            test.getNestedArray()[a][b].getStringTest().append("arrayDim:" + a + b);
+         }
+      }
+
+      for (int i = 0; i < 4; i++)
+      {
+         test.getStringArray()[i].append("arrayDim:" + i);
+      }
+
+      for (int s = 1; s < 4; s++)
+      {
+         for (int w = 0; w < 5; w++)
+         {
+            int i = s + s * w;
+
+            switch (s)
+            {
+            case 1:
+               test.getCharSeqTest().add(String.valueOf(i).charAt(0));
+               test.getWcharSeqTest().add('Ω');
+               test.getOctetSeqTest().add((byte) (i * 2));
+               test.getShortSeqTest().add((short) (-2 * i + 1));
+               test.getBooleanSeqTest().add(i % 2 == 0);
+            case 2:
+               test.getUshortSeqTest().add(i + 4);
+               test.getLongSeqTest().add(i * 124 - 98);
+               test.getUlongSeqTest().add(i * 11561);
+               test.getLonglongSeqtest().add(-2143125l + i * 1251);
+            case 3:
+               test.getUlonglongSeqTest().add(241l + i * 100421410l);
+               test.getFloatSeqTest().add(325.25f * i);
+               test.getDoubleSeqTest().add(15095.921 * i);
+               NestedElement elem = test.getNestedSeqTest().add();
+               elem.setLongTest(i * 541);
+               elem.getStringTest().append("dim:" + s + w);
+               StringBuilder builder = test.getStringSeqTest().add();
+               builder.append("sdim:" + s + w);
+
+            }
+         }
+      }
+   }
+
+   @Test
+   public void testIDLElementSerializeDeserialize() throws IOException
+   {
+      IDLElementTest test = new IDLElementTest();
+      populateIDLElementTest(test);
+
       IDLElementTestPubSubType type = new IDLElementTestPubSubType();
       SerializedPayload payload = new SerializedPayload(type.getTypeSize());
       type.serialize(test, payload);
 
-      
-      
       IDLElementTest other = new IDLElementTest();
       type.deserialize(payload, other);
-      System.out.println(other.toString());
       assertTrue(test.equals(other));
+
+   }
+
+   @Test
+   public void testIDLElementWithFastRTPSGenCPPCode() throws IOException
+   {
+      IDLElementTest test = new IDLElementTest();
+      populateIDLElementTest(test);
+
+      IDLElementTestPubSubType type = new IDLElementTestPubSubType();
+      SerializedPayload payload = new SerializedPayload(type.getTypeSize());
+      type.serialize(test, payload);
+
+      IDLElementTest cppElement = new IDLElementTest();
+
+      SerializedPayload cppPayload = new SerializedPayload(type.getTypeSize());
+      cppPayload.getData().put(IDLElementTestCPPData.cppData);
+      cppPayload.getData().flip();
+      cppPayload.setLength(IDLElementTestCPPData.cppData.length);
+      System.out.println();
+      type.deserialize(cppPayload, cppElement);
+      System.out.println(cppElement);
+
+      assertTrue(test.equals(cppElement));
    }
 }
