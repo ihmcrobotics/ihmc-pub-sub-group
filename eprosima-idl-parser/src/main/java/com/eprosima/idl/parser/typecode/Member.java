597a30c6eaf067f82ea1c447d5320da1b9e6ee9f
Duncan Calvert <dcalvert@ihmc.us>
2018-02-22 13:41:15 -0600

    Move eprosima-idl-parser into ihmc-pub-sub since it is now so heavily modified.

---
+++ eprosima-idl-parser/src/main/java/com/eprosima/idl/parser/typecode/Member.java
@@ -0,0 +1,96 @@
+// Copyright 2016 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.eprosima.idl.parser.typecode;
+
+import com.eprosima.idl.parser.tree.Annotation;
+import com.eprosima.idl.parser.tree.Notebook;
+import com.eprosima.idl.context.Context;
+import us.ihmc.commons.FormattingTools;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class Member implements Notebook
+{
+    public Member()
+    {
+        m_annotations = new HashMap<String, Annotation>();
+    }
+    
+    public Member(TypeCode typecode, String name)
+    {
+        m_typecode = typecode;
+        m_name = name;
+        m_annotations = new HashMap<String, Annotation>();
+    }
+    
+    public String getName()
+    {
+        return m_name;
+    }
+    
+    public String getJavaName() {
+    	if (m_name != null) {
+
+    	    // @dcalvert: Fix Java method naming from getSome_type() to getSomeType()
+    	   return FormattingTools.underscoredToCamelCase(m_name, true);
+//	    	Character firstChar =Character.toUpperCase(m_name.charAt(0));
+//	    	String javaName = firstChar.toString();
+//	    	if (m_name.length() > 1) {
+//	    		javaName += m_name.substring(1);
+//	    	}
+//	    	return javaName;
+    	}
+    	return null;
+    }
+
+    /*
+     * @brief This function is used with RTI DDS types because array names contains [].
+     */
+    
+    public TypeCode getTypecode()
+    {
+        return m_typecode;
+    }
+    
+    public void setName(String name)
+    {
+        m_name = name;
+    }
+    
+    public void setTypecode(TypeCode typecode)
+    {
+         m_typecode = typecode;
+    }
+
+    @Override
+    public void addAnnotation(Context ctx, Annotation annotation)
+    {
+        if(annotation != null)
+            m_annotations.put(annotation.getName(), annotation);
+    }
+
+    @Override
+    public Map<String, Annotation> getAnnotations()
+    {
+        return m_annotations;
+    }
+    
+    private String m_name = null;
+    
+    private TypeCode m_typecode = null;
+
+    private HashMap<String, Annotation> m_annotations = null;
+}
---
+++ eprosima-idl-parser/src/main/java/com/eprosima/idl/parser/typecode/StructTypeCode.java
@@ -0,0 +1,99 @@
+// Copyright 2016 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.eprosima.idl.parser.typecode;
+
+import com.eprosima.idl.util.Pair;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.antlr.stringtemplate.StringTemplate;
+
+public class StructTypeCode extends MemberedTypeCode
+{
+    public StructTypeCode(String scope, String name)
+    {
+        super(TypeCode.KIND_STRUCT, scope, name);
+    }
+    
+    @Override
+    public String getCppTypename()
+    {
+        StringTemplate st = getCppTypenameFromStringTemplate();
+        st.setAttribute("name", getScopedname());
+        return st.toString();
+    }
+
+    @Override
+    public String getJavaTypename()
+    {
+        StringTemplate st = getJavaTypenameFromStringTemplate();
+        st.setAttribute("name", getJavaScopedname());
+        return st.toString();
+    }
+    
+    @Override
+    public String getIdlTypename()
+    {
+        StringTemplate st = getIdlTypenameFromStringTemplate();
+        st.setAttribute("name", getScopedname());
+        return st.toString();
+    }
+    
+    @Override
+    public boolean isIsType_a(){
+    	return true;
+    }
+    
+    /*public Pair<Integer, Integer> getMaxSerializedSize(int currentSize, int lastDataAligned)
+    {
+        List<Member> members = getMembers();
+        int lcurrentSize = currentSize;
+        int llastDataAligned = lastDataAligned;
+        
+        for(int count = 0; count < members.size(); ++count)
+        {
+            Pair<Integer, Integer> pair = members.get(count).getTypecode().getMaxSerializedSize(lcurrentSize, llastDataAligned);
+            lcurrentSize = pair.first();
+            llastDataAligned = pair.second();
+        }
+        
+        return new Pair<Integer, Integer>(lcurrentSize, llastDataAligned);
+    }
+    
+    public int getMaxSerializedSizeWithoutAlignment(int currentSize)
+    {
+        List<Member> members = getMembers();
+        int lcurrentSize = currentSize;
+        
+        for(int count = 0; count < members.size(); ++count)
+        {
+            lcurrentSize = members.get(count).getTypecode().getMaxSerializedSizeWithoutAlignment(lcurrentSize);
+        }
+        
+        return lcurrentSize;
+    }
+    
+    public String getMaxSerializedSize()
+    {
+        Pair<Integer, Integer> pair = getMaxSerializedSize(0, 0);
+        return pair.first().toString();
+    }
+    
+    public String getMaxSerializedSizeWithoutAlignment()
+    {
+        return Integer.toString(getMaxSerializedSizeWithoutAlignment(0));
+    }*/
+}
