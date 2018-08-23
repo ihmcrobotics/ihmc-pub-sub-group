c1378d15b021f9d3fa97353af58ec64f11334a3d
Duncan Calvert <dcalvert@ihmc.us>
2018-08-23 16:12:16 -0500

    Format only cleanup.

--- ihmc-pub-sub-generator/src/test/java/us/ihmc/pubsub/tools/ParticipantListenerImpl.java
+++ ihmc-pub-sub-generator/src/test/java/us/ihmc/pubsub/tools/ParticipantListenerImpl.java
@@ -5,13 +5,13 @@ import us.ihmc.pubsub.participant.ParticipantDiscoveryInfo;
 import us.ihmc.pubsub.participant.ParticipantListener;
 
 public class ParticipantListenerImpl implements ParticipantListener
+{
+   @Override
+   public void onParticipantDiscovery(Participant participant, ParticipantDiscoveryInfo info)
    {
-      @Override
-      public void onParticipantDiscovery(Participant participant, ParticipantDiscoveryInfo info)
-      {
-         System.out.println("New participant discovered");
-         System.out.println("Status: " + info.getStatus());
-         System.out.println("Guid: " + info.getGuid().toString());
-         System.out.println("Name: " + info.getName());
-      }
+      System.out.println("New participant discovered");
+      System.out.println("Status: " + info.getStatus());
+      System.out.println("Guid: " + info.getGuid().toString());
+      System.out.println("Name: " + info.getName());
    }
+}
