82c388b27538f899d2e5706f015742d7948a5fe5
Doug Stephen <dstephen@ihmc.us>
2019-12-02 16:50:24 -0600

    Fast-RTPS native layer updated to be API compatible w/ new Fast-RTPS release.
    Still have linking/ABI issues.

--- ihmc-pub-sub/cppsrc/FastRTPS/nativeparticipantimpl.cpp
+++ ihmc-pub-sub/cppsrc/FastRTPS/nativeparticipantimpl.cpp
@@ -21,6 +21,7 @@
 
 #include <fastrtps/rtps/builtin/data/WriterProxyData.h>
 #include <fastrtps/rtps/builtin/data/ReaderProxyData.h>
+#include <fastrtps/rtps/reader/StatefulReader.h>
 
 
 using namespace us::ihmc::rtps::impl::fastRTPS;
@@ -92,82 +93,18 @@ void NativeParticipantImpl::MyRTPSParticipantListener::onRTPSParticipantDiscover
 {
     if(this->mp_participantimpl->listener!=nullptr)
     {
-
-        RTPSParticipantDiscoveryInfo& rtpsinfo = info.rtps;
+        ParticipantProxyData proxy_data = info.info;
 
         logInfo(PARTICIPANT,"Remote participant Guid: " << rtpsinfo.m_guid);
         GuidUnion retGuid;
-        CommonFunctions::guidcpy(rtpsinfo.m_guid, &retGuid);
+        CommonFunctions::guidcpy(proxy_data.m_guid, &retGuid);
 
-        this->mp_participantimpl->listener->onParticipantDiscovery((int64_t)&rtpsinfo, retGuid.primitive.high, retGuid.primitive.low, rtpsinfo.m_status);
+        this->mp_participantimpl->listener->onParticipantDiscovery((int64_t)&proxy_data, retGuid.primitive.high, retGuid.primitive.low, info.status);
     }
 }
 
 
-std::string NativeParticipantListener::getName(int64_t infoPtr)
+fixed_string<255> NativeParticipantListener::getName(int64_t infoPtr)
 {
-    return ((RTPSParticipantDiscoveryInfo*) infoPtr)->m_RTPSParticipantName;
-}
-
-void NativeParticipantImpl::registerEDPReaderListeners(NativeParticipantPublisherEDPListener *publisherListener, NativeParticipantSubscriberEDPListener *subscriberListener) throw(FastRTPSException)
-{
-    std::pair<StatefulReader*,StatefulReader*> EDP_Readers = this->part->getEDPReaders();
-    if(EDP_Readers.first == nullptr || EDP_Readers.second == nullptr)
-    {
-        throw FastRTPSException("No Endpoint Discovery Protocol Readers provided, Participant is connected in static mode.");
-    }
-
-    if(publisherListener != nullptr)
-    {
-        EDP_Readers.second->setListener(publisherListener->getReaderListener());
-    }
-    if(subscriberListener != nullptr)
-    {
-        EDP_Readers.first->setListener(subscriberListener->getReaderListener());
-    }
-}
-
-void NativeParticipantPublisherEDPListener::MyRTPSReaderListener::onNewCacheChangeAdded(RTPSReader *reader, const CacheChange_t * const change_in)
-{
-    CacheChange_t* change = (CacheChange_t*) change_in;
-    if(change->kind == ALIVE){
-
-        WriterProxyData proxyData;
-        CDRMessage_t tempMsg;
-        tempMsg.msg_endian = change->serializedPayload.encapsulation == PL_CDR_BE ? BIGEND:LITTLEEND;
-        tempMsg.length = change->serializedPayload.length;
-        memcpy(tempMsg.buffer,change->serializedPayload.data,tempMsg.length);
-        if(proxyData.readFromCDRMessage(&tempMsg)){
-
-            GuidUnion guid;
-            CommonFunctions::guidcpy(proxyData.guid(), &guid);
-            GuidUnion participantGuid;
-            CommonFunctions::guidcpy(proxyData.RTPSParticipantKey(), &participantGuid);
-
-            mp_listener->publisherTopicChange(proxyData.isAlive(), guid.primitive.high, guid.primitive.low, &proxyData.unicastLocatorList(), &proxyData.multicastLocatorList(), participantGuid.primitive.high, participantGuid.primitive.low,
-                                              proxyData.typeName(), proxyData.topicName(), proxyData.userDefinedId(), proxyData.typeMaxSerialized(), proxyData.topicKind(), &proxyData.m_qos);
-        }
-    }
-}
-
-void NativeParticipantSubscriberEDPListener::MyRTPSReaderListener::onNewCacheChangeAdded(RTPSReader* reader, const CacheChange_t* const change_in)
-{
-    CacheChange_t* change = (CacheChange_t*) change_in;
-    if(change->kind == ALIVE){
-
-        ReaderProxyData proxyData;
-        CDRMessage_t tempMsg;
-        tempMsg.msg_endian = change->serializedPayload.encapsulation == PL_CDR_BE ? BIGEND:LITTLEEND;
-        tempMsg.length = change->serializedPayload.length;
-        memcpy(tempMsg.buffer,change->serializedPayload.data,tempMsg.length);
-        if(proxyData.readFromCDRMessage(&tempMsg)){
-            GuidUnion guid;
-            CommonFunctions::guidcpy(proxyData.guid(), &guid);
-            GuidUnion participantGuid;
-            CommonFunctions::guidcpy(proxyData.RTPSParticipantKey(), &participantGuid);
-
-            mp_listener->subscriberTopicChange(proxyData.isAlive(), guid.primitive.high, guid.primitive.low, proxyData.m_expectsInlineQos, &proxyData.unicastLocatorList(), &proxyData.multicastLocatorList(), participantGuid.primitive.high, participantGuid.primitive.low,
-                                               proxyData.typeName(), proxyData.topicName(), proxyData.userDefinedId(), proxyData.topicKind(), &proxyData.m_qos);
-        }
-    }
+    return ((ParticipantProxyData*) infoPtr)->m_participantName;
 }
