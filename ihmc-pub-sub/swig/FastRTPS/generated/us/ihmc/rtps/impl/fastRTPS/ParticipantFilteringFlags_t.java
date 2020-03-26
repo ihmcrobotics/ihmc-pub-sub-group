411c67ed66f4d9b51d23c0cda72c338519896a8d
Jesper Smith <jesper@halodi.com>
2020-03-26 19:43:17 +0100

    Compiled C++ code

--- ihmc-pub-sub/swig/FastRTPS/generated/us/ihmc/rtps/impl/fastRTPS/ParticipantFilteringFlags_t.java
+++ ihmc-pub-sub/swig/FastRTPS/generated/us/ihmc/rtps/impl/fastRTPS/ParticipantFilteringFlags_t.java
@@ -8,11 +8,11 @@
 
 package us.ihmc.rtps.impl.fastRTPS;
 
-public final class DISCOVERY_STATUS {
-  public final static DISCOVERY_STATUS DISCOVERED_RTPSPARTICIPANT = new DISCOVERY_STATUS("DISCOVERED_RTPSPARTICIPANT");
-  public final static DISCOVERY_STATUS CHANGED_QOS_RTPSPARTICIPANT = new DISCOVERY_STATUS("CHANGED_QOS_RTPSPARTICIPANT");
-  public final static DISCOVERY_STATUS REMOVED_RTPSPARTICIPANT = new DISCOVERY_STATUS("REMOVED_RTPSPARTICIPANT");
-  public final static DISCOVERY_STATUS DROPPED_RTPSPARTICIPANT = new DISCOVERY_STATUS("DROPPED_RTPSPARTICIPANT");
+public final class ParticipantFilteringFlags_t {
+  public final static ParticipantFilteringFlags_t NO_FILTER = new ParticipantFilteringFlags_t("NO_FILTER", 0);
+  public final static ParticipantFilteringFlags_t FILTER_DIFFERENT_HOST = new ParticipantFilteringFlags_t("FILTER_DIFFERENT_HOST", 0x1);
+  public final static ParticipantFilteringFlags_t FILTER_DIFFERENT_PROCESS = new ParticipantFilteringFlags_t("FILTER_DIFFERENT_PROCESS", 0x2);
+  public final static ParticipantFilteringFlags_t FILTER_SAME_PROCESS = new ParticipantFilteringFlags_t("FILTER_SAME_PROCESS", 0x4);
 
   public final int swigValue() {
     return swigValue;
@@ -22,33 +22,33 @@ public final class DISCOVERY_STATUS {
     return swigName;
   }
 
-  public static DISCOVERY_STATUS swigToEnum(int swigValue) {
+  public static ParticipantFilteringFlags_t swigToEnum(int swigValue) {
     if (swigValue < swigValues.length && swigValue >= 0 && swigValues[swigValue].swigValue == swigValue)
       return swigValues[swigValue];
     for (int i = 0; i < swigValues.length; i++)
       if (swigValues[i].swigValue == swigValue)
         return swigValues[i];
-    throw new IllegalArgumentException("No enum " + DISCOVERY_STATUS.class + " with value " + swigValue);
+    throw new IllegalArgumentException("No enum " + ParticipantFilteringFlags_t.class + " with value " + swigValue);
   }
 
-  private DISCOVERY_STATUS(String swigName) {
+  private ParticipantFilteringFlags_t(String swigName) {
     this.swigName = swigName;
     this.swigValue = swigNext++;
   }
 
-  private DISCOVERY_STATUS(String swigName, int swigValue) {
+  private ParticipantFilteringFlags_t(String swigName, int swigValue) {
     this.swigName = swigName;
     this.swigValue = swigValue;
     swigNext = swigValue+1;
   }
 
-  private DISCOVERY_STATUS(String swigName, DISCOVERY_STATUS swigEnum) {
+  private ParticipantFilteringFlags_t(String swigName, ParticipantFilteringFlags_t swigEnum) {
     this.swigName = swigName;
     this.swigValue = swigEnum.swigValue;
     swigNext = this.swigValue+1;
   }
 
-  private static DISCOVERY_STATUS[] swigValues = { DISCOVERED_RTPSPARTICIPANT, CHANGED_QOS_RTPSPARTICIPANT, REMOVED_RTPSPARTICIPANT, DROPPED_RTPSPARTICIPANT };
+  private static ParticipantFilteringFlags_t[] swigValues = { NO_FILTER, FILTER_DIFFERENT_HOST, FILTER_DIFFERENT_PROCESS, FILTER_SAME_PROCESS };
   private static int swigNext = 0;
   private final int swigValue;
   private final String swigName;
