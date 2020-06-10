ab013d16f611de22dc7ba834be431c1a77646f53
Duncan Calvert <dcalvert@ihmc.us>
2020-06-10 12:38:44 -0500

    :arrow_up: ihmc-commons 0.30.0

--- ihmc-pub-sub/build.gradle.kts
+++ ihmc-pub-sub/build.gradle.kts
@@ -20,7 +20,7 @@ mainDependencies {
    api("us.ihmc:ihmc-native-library-loader:1.2.1")
    api("net.sf.trove4j:trove4j:3.0.3")
    api("us.ihmc:euclid:0.14.2")
-   api("us.ihmc:ihmc-commons:0.29.0")
+   api("us.ihmc:ihmc-commons:0.30.0")
 }
 
 testDependencies {
