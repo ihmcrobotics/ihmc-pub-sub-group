ed2f378a7d447c2b49e2cceb2bb2af1c56f85ec7
Duncan Calvert <dcalvert@ihmc.us>
2020-12-01 14:14:54 -0600

    :arrow_up: Run upgrade dependencies

--- ihmc-pub-sub/build.gradle.kts
+++ ihmc-pub-sub/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
    id("us.ihmc.ihmc-build")
    id("us.ihmc.ihmc-ci") version "7.4"
-   id("us.ihmc.ihmc-cd") version "1.14"
+   id("us.ihmc.ihmc-cd") version "1.17"
    id("com.github.hierynomus.license") version "0.14.0"
 }
 
@@ -20,9 +20,9 @@ mainDependencies {
    api("us.ihmc:ihmc-native-library-loader:1.2.1")
    api("net.sf.trove4j:trove4j:3.0.3")
    api("us.ihmc:euclid:0.15.1")
-   api("us.ihmc:ihmc-commons:0.30.2")
+   api("us.ihmc:ihmc-commons:0.30.4")
 }
 
 testDependencies {
-   api("us.ihmc:ihmc-commons-testing:0.30.2")
+   api("us.ihmc:ihmc-commons-testing:0.30.4")
 }
