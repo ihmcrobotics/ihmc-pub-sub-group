38162c60160ba235268bf2a51381455cfc6e2892
Sylvain Bertrand <sbertrand@ihmc.us>
2020-12-29 16:11:35 -0600

    :arrow_up: euclid 0.16.1

--- ihmc-pub-sub/build.gradle.kts
+++ ihmc-pub-sub/build.gradle.kts
@@ -19,7 +19,7 @@ ihmc {
 mainDependencies {
    api("us.ihmc:ihmc-native-library-loader:1.3.1")
    api("net.sf.trove4j:trove4j:3.0.3")
-   api("us.ihmc:euclid:0.16.0")
+   api("us.ihmc:euclid:0.16.1")
    api("us.ihmc:ihmc-commons:0.30.4")
 }
 
