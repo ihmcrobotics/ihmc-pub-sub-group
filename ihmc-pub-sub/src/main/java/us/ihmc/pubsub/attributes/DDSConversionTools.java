ebe5df31f6986992ab2026cfa25e0d6e04c1bf13
Jesper Smith <jesper@halodi.com>
2022-06-10 12:13:06 +0200

    Added documentation

--- ihmc-pub-sub/src/main/java/us/ihmc/pubsub/attributes/DDSConversionTools.java
+++ ihmc-pub-sub/src/main/java/us/ihmc/pubsub/attributes/DDSConversionTools.java
@@ -10,8 +10,21 @@ import com.eprosima.xmlschemas.fastrtps_profiles.DurationType;
 import us.ihmc.pubsub.common.Time;
 import us.ihmc.pubsub.impl.fastRTPS.FastRTPSDomain;
 
+/**
+ * Helper class to convert between different formats inside the DDS layer
+ * 
+ *  Mostly used for Time
+ *  
+ * @author jesper
+ *
+ */
 public class DDSConversionTools
 {
+   /**
+    * Convert Time to the XML durationType
+    * @param Time
+    * @return DurationType
+    */
    public static DurationType timeToDurationType(Time time)
    {
       DurationType dt = new DurationType();
@@ -22,6 +35,12 @@ public class DDSConversionTools
       return dt;
    }
 
+   /**
+    * Convert an XML duration type to Time 
+    * 
+    * @param duration
+    * @return Time
+    */
    public static Time durationTypeToTime(DurationType duration)
    {
       if(duration == null)
@@ -43,5 +62,24 @@ public class DDSConversionTools
          }
       }
       return time;
+   }
+   
+   /**
+    * Create a time element based on time in seconds
+    * 
+    * @param timeInSeconds
+    * @return Time for DDS
+    */
+   public Time createTime(double timeInSeconds)
+   {
+      Time time = new Time();
+      int seconds = (int) timeInSeconds;
+      double remainder = timeInSeconds - seconds;
+            
+      time.setSeconds((int) timeInSeconds);
+      time.setNanoseconds((long)(remainder * 1e9));
+      
+      return time;
+
    }
 }
