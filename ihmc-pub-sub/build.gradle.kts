d9a10d23e758f964a3bd62c37e6c12f1c6200e9a
Dexton Anderson <danderson@ihmc.org>
2023-01-05 10:52:51 -0600

    :arrow_up: ihmc-native-library-loader 2.0.2
    Former-commit-id: cf1ac5c5ac3d720ab84a14e40f1ebd8b43204f24

--- ihmc-pub-sub/build.gradle.kts
+++ ihmc-pub-sub/build.gradle.kts
@@ -20,7 +20,7 @@ ihmc {
 //buildDir = 'build-java'
 
 mainDependencies {
-   api("us.ihmc:ihmc-native-library-loader:2.0.1")
+   api("us.ihmc:ihmc-native-library-loader:2.0.2")
    api("net.sf.trove4j:trove4j:3.0.3")
    api("us.ihmc:euclid:0.19.1")
    api("us.ihmc:ihmc-commons:0.32.0")
