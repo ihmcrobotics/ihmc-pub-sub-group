6fee8d35249d4bc758cd686a5a07a046d7e972c1
Jesper Smith <jesper@halodi.com>
2023-03-28 16:22:39 +0200

    Cleanup for merge
    Former-commit-id: 519eac68997ff7d8c71a8d593538eb4053adb424

--- ihmc-pub-sub/build.gradle.kts
+++ ihmc-pub-sub/build.gradle.kts
@@ -9,9 +9,6 @@ plugins {
 
 ihmc {
    loadProductProperties("../group.gradle.properties")
-   
-   repository("https://artifacts.halodi.com/repository/maven-open-source-group")
-   
 
    configureDependencyResolution()
    javaDirectory("main", "../../swig/FastRTPS/generated")
@@ -23,7 +20,7 @@ ihmc {
 //buildDir = 'build-java'
 
 mainDependencies {
-   api("us.ihmc:ihmc-native-library-loader:2.0.0-halodi2")
+   api("us.ihmc:ihmc-native-library-loader:2.0.2")
    api("net.sf.trove4j:trove4j:3.0.3")
    api("us.ihmc:euclid:0.19.1")
    api("us.ihmc:ihmc-commons:0.32.0")
