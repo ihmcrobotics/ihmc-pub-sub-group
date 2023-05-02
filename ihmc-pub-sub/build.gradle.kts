8d471e3302598eae4133bea656d29ea054284e34
Duncan Calvert <dcalvert@ihmc.us>
2023-05-02 11:08:31 -0500

    :arrow_up: euclid 0.20.0
    Former-commit-id: 14a09d580bc970b4d01b7dd0d8213bf2192d5b65

--- ihmc-pub-sub/build.gradle.kts
+++ ihmc-pub-sub/build.gradle.kts
@@ -22,7 +22,7 @@ ihmc {
 mainDependencies {
    api("us.ihmc:ihmc-native-library-loader:2.0.2")
    api("net.sf.trove4j:trove4j:3.0.3")
-   api("us.ihmc:euclid:0.19.1")
+   api("us.ihmc:euclid:0.20.0")
    api("us.ihmc:ihmc-commons:0.32.0")
    api("us.ihmc:log-tools:0.6.3")
    api("jakarta.xml.bind:jakarta.xml.bind-api:2.3.2")
