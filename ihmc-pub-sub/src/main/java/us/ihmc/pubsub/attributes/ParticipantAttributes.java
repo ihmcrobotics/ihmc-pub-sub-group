61492600549d2aaa0e09d57fdf1f5df35c445e68
Dexton Anderson <danderson@ihmc.org>
2023-05-11 16:25:17 -0500

    Do not set maxMessageSize
    Former-commit-id: e6ae5f3507e80e6f4ba0c50791aca04e6fbfcf00

--- ihmc-pub-sub/src/main/java/us/ihmc/pubsub/attributes/ParticipantAttributes.java
+++ ihmc-pub-sub/src/main/java/us/ihmc/pubsub/attributes/ParticipantAttributes.java
@@ -165,13 +165,7 @@ public class ParticipantAttributes
          RtpsTransportDescriptorType transportDescriptor = new RtpsTransportDescriptorType();
          transportDescriptor.setTransportId(transportName);
          transportDescriptor.setType("UDPv4");
-         // Set the max message size equal to the standard Ethernet MTU (1500 bytes)
-         // This prevents fragmentation of large messages at the network layer, leaving that to Fast-DDS to handle
-         // The default value is 65500 bytes.
-         // See: https://fast-dds.docs.eprosima.com/en/latest/fastdds/xml_configuration/transports.html
-         // See: https://github.com/eProsima/Fast-DDS/issues/3053
-         // See: https://en.wikipedia.org/wiki/Maximum_transmission_unit
-         transportDescriptor.setMaxMessageSize(1500L);
+
          AddressListType addressWhitelist = new AddressListType();
 
          for (InetAddress addr : bindToAddressRestrictions)
