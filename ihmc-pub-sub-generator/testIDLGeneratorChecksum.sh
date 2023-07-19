adc0f82e75847ed1ca7bbac1bbdb459fa987806b
Jesper Smith <jesper@halodi.com>
2023-07-19 17:34:02 +0200

    Add testing files for the checksum
    Former-commit-id: 6ab2382648d6266f1a19db9c7f7e01b3e3743f64

---
+++ ihmc-pub-sub-generator/testIDLGeneratorChecksum.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+gradle distTar
+
+tar xvf build/distributions/ihmc-pub-sub-generator-0.18.2.tar -C build
+
+IDL_GENERATOR_OPTS="-Ddebug=true" build/ihmc-pub-sub-generator-0.18.2/bin/IDLGenerator src/test/idl/IDLElementTest.idl test_msgs build
