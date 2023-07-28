9c0541ab8c02e20ec4dbca1b4075abdb757e547f
Jesper Smith <jesper@halodi.com>
2023-07-28 16:40:08 +0200

    Cleanup logging
    Former-commit-id: b8eb3711e09e9fcd93c98eab3f55fe4b852384be

--- ihmc-pub-sub-generator/testIDLGeneratorChecksum.sh
+++ ihmc-pub-sub-generator/testIDLGeneratorChecksum.sh
@@ -4,4 +4,4 @@ gradle distTar
 
 tar xvf build/distributions/ihmc-pub-sub-generator-0.18.2.tar -C build
 
-IDL_GENERATOR_OPTS="-Ddebug-idl=true" build/ihmc-pub-sub-generator-0.18.2/bin/IDLGenerator src/test/idl/IDLElementTest.idl test_msgs build
+IDL_GENERATOR_OPTS="-Dwrite-preprocessed-checksum-idl=true -Dlog.level=debug" build/ihmc-pub-sub-generator-0.18.2/bin/IDLGenerator src/test/idl/IDLElementTest.idl test_msgs build
