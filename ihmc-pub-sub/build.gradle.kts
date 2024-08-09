0afd1dcce1c36a60b6e1636f75ea8da743b86006
Duncan Calvert <dcalvert@ihmc.org>
2024-08-09 13:51:07 -0500

    :arrow_up: ihmc-native-library-loader 2.0.3

--- ihmc-pub-sub/build.gradle.kts
+++ ihmc-pub-sub/build.gradle.kts
@@ -20,7 +20,7 @@ ihmc {
 //buildDir = 'build-java'
 
 mainDependencies {
-   api("us.ihmc:ihmc-native-library-loader:2.0.2")
+   api("us.ihmc:ihmc-native-library-loader:2.0.3")
    api("net.sf.trove4j:trove4j:3.0.3")
    api("us.ihmc:euclid:0.21.0")
    api("us.ihmc:ihmc-commons:0.32.0")
